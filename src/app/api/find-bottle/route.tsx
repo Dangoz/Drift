dc7cafe8c429ad01c93e057f51a5f08e20d26bad
Dango <jason543211@hotmail.com>
2024-03-24 02:22:13 -0700

    fix: empty route

---
+++ src/app/api/find-bottle/route.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-key */
+import { createFrames, Button } from 'frames.js/next'
+
+const frames = createFrames()
+
+const handleRequest = frames(async (ctx) => {
+  console.log('message', ctx.message || 'no message')
+
+  return {
+    image: (
+      <div tw="flex flex-col">
+        <div tw="flex">Reading the bottle 0-0</div>
+      </div>
+    ),
+    imageOptions: {
+      aspectRatio: '1:1',
+    },
+    buttons: [
+      <Button
+        action="post"
+        target={
+          {
+            // query: { pageIndex: (pageIndex - 1) % totalPages },
+          }
+        }
+      >
+        ←←
+      </Button>,
+      <Button
+        action="post"
+        target={
+          {
+            // query: { pageIndex: (pageIndex + 1) % totalPages },
+          }
+        }
+      >
+        →→
+      </Button>,
+    ],
+    textInput: 'Type something!',
+  }
+})
+
+export const POST = handleRequest

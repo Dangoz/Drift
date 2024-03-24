0b39ecff620917cdc521d8c077f923c63a240f6c
Dango <jason543211@hotmail.com>
2024-03-24 03:30:45 -0700

    chore: find-bottle menu

--- src/app/api/find-bottle/route.tsx
+++ src/app/api/find-bottle/route.tsx
@@ -1,47 +1,58 @@
 /* eslint-disable react/jsx-key */
 import { createFrames, Button } from 'frames.js/next'
 import { getRandomBottle } from '@/model/bottle'
+import { EMPTY_SCROLL_BACKGROUND } from '@/common/constants'
+import { getFarcasterUserByFID } from '@/common/pinata'
 
 const frames = createFrames()
 
 const handleRequest = frames(async (ctx) => {
   const randomBottle = await getRandomBottle()
-  console.log('randomBottle', randomBottle)
-
-  console.log('message', ctx.message || 'no message')
+  const author = await getFarcasterUserByFID(randomBottle.authorFID)
 
   return {
     image: (
-      <div tw="flex flex-col">
-        <div tw="flex">Reading the bottle 0-0</div>
+      <div tw="flex w-full h-full flex-col justify-center items-center">
+        {/* background */}
+
+        {/* author */}
+        <div tw="flex flex-col w-full h-40 items-start justify-center text-white z-10 absolute top-0 left-1">
+          {/* <img src={author.pfp_url} alt="Empty Scroll Background" tw="w-8 h-8 rounded-full mr-2" /> */}
+          {/* instead of showing the avatar, show a grey placeholder of the same style */}
+          <div tw="w-8 h-8 rounded-full mr-2 bg-gray-300" />
+          {/* <div tw="text-lg font-bold text-purple-500">{author.display_name}</div> */}
+          <div tw="w-16 h-4 bg-gray-300 mt-1" />
+          <div tw="text-black text-sm break-words">{randomBottle.message}</div>
+        </div>
       </div>
     ),
     imageOptions: {
       aspectRatio: '1:1',
+      width: 300,
+      height: 300,
     },
     buttons: [
       <Button
         action="post"
-        target={
-          {
-            // query: { pageIndex: (pageIndex - 1) % totalPages },
-          }
-        }
+        target={{
+          query: {
+            authorFID: randomBottle.authorFID,
+            message: randomBottle.message,
+          },
+          pathname: '/api/send-bottle',
+        }}
       >
-        ←←
+        Reply 📝
       </Button>,
       <Button
         action="post"
-        target={
-          {
-            // query: { pageIndex: (pageIndex + 1) % totalPages },
-          }
-        }
+        target={{
+          pathname: '/api/main-menu',
+        }}
       >
-        →→
+        Throw Away 🌊
       </Button>,
     ],
-    textInput: 'Type something!',
   }
 })
 
